845c90e3f4e624efbab12cf5961c412659cfe9fe
Shkila-coder <114057059+Shkila-coder@users.noreply.github.com>
2023-06-30 19:34:01 +0300

    Add files via upload

---
+++ RandomFigures/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RandomFigures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphWind = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.graphWind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphWind
+            // 
+            this.graphWind.BackColor = System.Drawing.Color.White;
+            this.graphWind.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.graphWind.Location = new System.Drawing.Point(28, 26);
+            this.graphWind.Name = "graphWind";
+            this.graphWind.Size = new System.Drawing.Size(731, 299);
+            this.graphWind.TabIndex = 0;
+            this.graphWind.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.graphWind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.graphWind)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphWind;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RandomFigures/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomFigures
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Создание "Художника" и привязка его к "холсту" grWind 
+            Graphics g = graphWind.CreateGraphics();
+            //Создание и установка цвета пера 
+            Pen myPen = new Pen(Color.Red);                //Красный цвет 
+            int nmbInterv = 100;                          //Количество точек на графике 
+            float xmin = 0, xmax =1000;                     //Диапазон изменения аргумента х 
+            float xstep = (xmax - xmin) / nmbInterv;      //Шаг по аргументу 
+            float ymin = xmin, ymax = 250;           //Диапазон изменения функции y= x^2 
+            //Коэффициенты пересчета абсолютных координат в координаты "холста" 
+            float kx = graphWind.Width / (xmax - xmin), ky = graphWind.Height / (ymax - ymin);
+            float x1 = xmin, y1 = ymin;                //Первая точка графика 
+            Font fnt = new Font("Arial", 30);  //Шрифт Arial, размер 10 
+            Random rnd = new Random();
+            int value = rnd.Next(6, 10); // случайное кол-во фигур
+
+
+            for (int i = 0; i < value; i++)
+            {
+                 value = rnd.Next(0, 5); // определяем номер фигуры случайно 
+                if (value == 0) //создание линии
+                {
+                    float x = rnd.Next(0, 1000);
+                    float y = rnd.Next(0, 250);
+                    float x_1 = rnd.Next(0, 1000);
+                    float y_1 = rnd.Next(0, 250);
+                    g.DrawLine(new Pen(Color.Black), x, y, x_1, y_1);
+                }
+                else if (value == 1) // создание круга
+                {
+                    float a = rnd.Next(0, 100);
+                    float x = rnd.Next(0, 1000);
+                    float y = rnd.Next(0, 250);
+                    g.DrawEllipse(new Pen(Color.Green), x, y, a, a);
+                }
+                else if (value == 2) // создание эллипсов
+                {
+                    float b= rnd.Next(0, 100);
+                    float a = rnd.Next(0, 100);
+                    float x = rnd.Next(0, 1000);
+                    float y = rnd.Next(0, 250);
+                    g.DrawEllipse(new Pen(Color.Yellow), x, y, a, b);
+                }
+                else if (value == 3) // создание прямоугольников
+                {
+                    float b = rnd.Next(0, 100);
+                    float a = rnd.Next(0, 100);
+                    float x = rnd.Next(0, 1000);
+                    float y = rnd.Next(0, 250);
+                    g.DrawRectangle(myPen, x, y, a, b);
+                }
+                else if (value == 4) // создание букв
+                {
+                    float b = rnd.Next(0, 1000);
+                    float a = rnd.Next(0, 100);
+                    char s = (char)(rnd.Next(1040, 1104));
+                    g.DrawString(Convert.ToString(s), fnt, new SolidBrush(Color.Blue), b, a);
+                }
+
+            }
+            //g.DrawRectangle(myPen, 10, 10, 100, 100);
+            //Вывод текста 
+           // Font fnt = new Font("Arial", 10);  //Шрифт Arial, размер 10 
+           // g.DrawString("y=Sin(2π∗x)∗exp(x) \r\n ", fnt, new SolidBrush(Color.Red), 10, 10);
+        }
+    }
+}
